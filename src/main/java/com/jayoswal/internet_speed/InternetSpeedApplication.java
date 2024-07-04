0ee80699d53666077bcf93bc843cb9e740ee662c
jayoswal <jayumeshoswal2001@gmail.com>
2024-07-04 16:19:06 +0530

    SpringBoot Project - MongoDB, removed Test

---
+++ src/main/java/com/jayoswal/internet_speed/InternetSpeedApplication.java
@@ -0,0 +1,13 @@
+package com.jayoswal.internet_speed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetSpeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetSpeedApplication.class, args);
+	}
+
+}
